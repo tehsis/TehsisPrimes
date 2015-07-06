229fac838daaef60da46a0f9899b0fe4e8e09000
Pablo Terradillos <tehsis@gmail.com>
2015-07-05 23:47:16 -0300

    added readme and test

---
+++ README.md
@@ -0,0 +1,4 @@
+### Description
+
+This is a simple class for generating primes using the Sieve of Eratosthenes with no more porpuses that test some C++ features.
+
--- tests/primes.cpp
+++ tests/primes.cpp
@@ -2,11 +2,16 @@
 #include <iostream>
 #include "../prime.h"
 using namespace std;
+using namespace Tehsis;
 
 #define test(a, m) assert(a); cout << m << endl;
 
 int main() {
-  Prime p(500);
+  Prime p(5);
+  Prime q(500);
+  Prime r(1024);
   
-  test(p == 499, "499 is the maximum prime number for 500");
+  test(p == 3, "3 is the maximum prime number for 5");
+  test(q == 499, "499 is the maximum prime number for 500");
+  test(r == 1021, "1021 is the maximum prime number for 1024");
 }
